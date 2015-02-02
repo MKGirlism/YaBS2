ad219beabd7475d8ad8dba8930430ebb4bae5753
Yamilla Hoshi <yamilla@yamihoshi.nl>
2015-02-02 11:36:37 +0100

    Better Footer.
    Seriously, I didn't know, this hasn't been Commited, yet!

--- module/foot.php
+++ module/foot.php
@@ -1,1 +1,17 @@
-<br /><br /></div><br />YaBS 2.0 Alpha, Blog Software by <a href="http://www.yamisoft.wtf">Yamisoft</a>.</div>
+<?php
+	$mysqli = new mysqli($hosty, $uname, $paswd, $dbnme);
+	
+	$sql = "SELECT `Name` FROM `General`";
+	$stmt = $mysqli->prepare($sql);
+echo $mysqli->error;
+	$stmt->execute();
+	
+	$stmt->bind_result($sname);
+	
+	while ($stmt->fetch()) {
+		echo "<br /><br />$sname, Blog Software by <a href='http://www.yamisoft.wtf'>Yamisoft</a>.";
+	}
+	
+	$stmt->close();
+	$mysqli->close();
+?>
